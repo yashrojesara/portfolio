fd09281576b939ee4e5b719b33737b525a93ddc5
Yash Rojesara <yashsoni9861@gmail.com>
2022-07-30 12:04:40 +0530

    Added routes for navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Blog from "./Components/Blogs/Blog";
+import Home from "./Components/Home/Home";
+import Project from "./Components/Projects/Project";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
