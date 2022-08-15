e40aa467cd5cce9afc0e2c2ca9840a6eb47dfe91
Yash Rojesara <yashsoni9861@gmail.com>
2022-08-15 10:10:18 +0530

    Added dummy description

--- src/helper.js
+++ src/helper.js
@@ -3,12 +3,17 @@ export const name = "Yash Rojesara";
 export const tabs = ["Home", "Projects", "Blogs"];
 
 export const AboutMe = `
-AboutMe.`;
+I've completed my graduation in 2019 from LDRP Gandhinagar. I'm
+working as a Software Developer at Tatvasoft-Ahmedabas since past 3
+years. Where I have developed many websites and web applications. I have a
+good knowledge of HTML, CSS, JavaScript, React, NodeJS, ExpressJS,
+MongoDB, MySQL, and Firebase.`;
+
 export const ProjectDescription = `This is some description about my project where I will write some
-            more things about that`;
+more things about that`;
 
 export const BlogDescription = `This is some description about my blog where I will write some more
-            things about that`;
+things about that`;
 
 export const KnownTechnologies = [
   {
