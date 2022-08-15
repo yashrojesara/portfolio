49201f1f90ca13890662a615512af89cac029d97
Yash Rojesara <yashsoni9861@gmail.com>
2022-08-15 15:53:57 +0530

    added description about myself, project & blogs

--- src/helper.js
+++ src/helper.js
@@ -2,18 +2,13 @@ export const name = "Yash Rojesara";
 
 export const tabs = ["Home", "Projects", "Blogs"];
 
-export const AboutMe = `
-I've completed my graduation in 2019 from LDRP Gandhinagar. I'm
-working as a Software Developer at Tatvasoft-Ahmedabas since past 3
-years. Where I have developed many websites and web applications. I have a
-good knowledge of HTML, CSS, JavaScript, React, NodeJS, ExpressJS,
-MongoDB, MySQL, and Firebase.`;
+export const AboutMe = `I have completed my graduation in computer engineering in 2019 from LDRP-ITR Gandhinagar College. Since last 3 years I'm working as a Software Developer in Tatvasoft-Ahemdabad.
+In past I have worked on many projects in different technologies like .Net, WPF, WCF, React, Nodejs, ExpressJS, & MySQL.
+`;
 
-export const ProjectDescription = `This is some description about my project where I will write some
-more things about that`;
+export const ProjectDescription = `I have worked on quite a few projects, I have added Github link as well as Live deployed link in each project. Do checkout that.`;
 
-export const BlogDescription = `This is some description about my blog where I will write some more
-things about that`;
+export const BlogDescription = `I have added some dummy blogs over here. Do check out that as well`;
 
 export const KnownTechnologies = [
   {
@@ -26,6 +21,6 @@ export const KnownTechnologies = [
   },
   {
     title: "Library/Framework",
-    list: ["ReactJS", "Redux", "NodeJS", "MaterialUI"],
+    list: ["ReactJS", "Redux", "NodeJS", "ExpressJS", "MaterialUI"],
   },
 ];
